aa2a698ab412e92f56bfc36fb67cff2ded50707a
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-11-07 19:00:36 +0200

    add fourth task

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -3,21 +3,24 @@ import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
-
-// Додати тип Menu Selected
+type SelectedMenu = { id: MenuIds };
+type MenuSelected = {
+  selectedMenu: SelectedMenu;
+};
+type MenuAction = {
+  onSelectedMenu: (menu: SelectedMenu) => void;
+};
 
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
 });
-
-// Додайте тип MenuAction
 
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +51,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
