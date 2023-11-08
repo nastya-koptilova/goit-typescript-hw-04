33b7e8e918a9dcfc4b3216f975cff5c193c3d76e
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-11-08 16:47:15 +0200

    fix

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -3,7 +3,7 @@ import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
-type SelectedMenu = { id: MenuIds };
+type SelectedMenu = { id?: MenuIds };
 type MenuSelected = {
   selectedMenu: SelectedMenu;
 };
